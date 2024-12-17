0fffa29920f3d788925d4c137242b0b97f518a00
Mehdi <khammali26@gmail.com>
2024-12-17 11:09:13 +0100

    setup Database

---
+++ database.php
@@ -0,0 +1,17 @@
+<?php
+$host = 'localhost';
+$username = 'root';
+$password = '123';
+$dbname = 'Blog_DB';
+
+$connection = new mysqli($host,$username,$password,$dbname);
+
+/*if (!$connection){
+    echo "connection failed";
+}else{
+    echo "connection good";
+}*/
+
+
+?>
+
