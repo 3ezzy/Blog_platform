7d1b13686df13297e5e7dbcda93e75f3bfbe52f5
Mehdi <khammali26@gmail.com>
2024-12-18 22:36:37 +0100

    ameliorate display username

--- index.php
+++ index.php
@@ -116,8 +116,9 @@ session_start();
                             <?php
                             
                             if (isset($_SESSION['username'])) {
+                                $username = $_SESSION['username'];
                                 // Display username if logged in
-                                echo '<span class="text-gray-800">Welcome, ' . htmlspecialchars($_SESSION['username']) . '!</span>';
+                                echo '<span class="text-gray-800">Welcome, ' . $username . '!</span>';
                             ?>
                                 <a class="p-2 w-full flex items-center text-sm text-gray-800 hover:text-gray-500 focus:outline-none focus:text-gray-500" href="Connexion/logout.php">
                                     <svg class="shrink-0 size-4 me-3 md:me-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
