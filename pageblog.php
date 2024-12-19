d7884eb3d7a35816429a7d01fd05b44beefa77e4
Mehdi <khammali26@gmail.com>
2024-12-19 12:08:28 +0100

    add page addblog

--- pageblog.php
+++ pageblog.php
@@ -72,7 +72,7 @@ session_start();
             <div id="hs-header-classic" class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block" aria-labelledby="hs-header-classic-collapse">
                 <div class="overflow-hidden overflow-y-auto max-h-[75vh] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300">
                     <div class="py-2 md:py-0 flex flex-col md:flex-row md:items-center md:justify-end gap-0.5 md:gap-1">
-                        <a class="p-2 flex items-center text-sm text-blue-600 focus:outline-none focus:text-blue-600" href="index.php" aria-current="page">
+                        <a class="p-2 flex items-center text-sm hover:text-gray-500 focus:outline-none focus:text-gray-500" href="index.php" aria-current="page">
                             <svg class="shrink-0 size-4 me-3 md:me-2 block md:hidden" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
                                 <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
@@ -80,7 +80,8 @@ session_start();
                             Home
                         </a>
 
-                        <a class="p-2 flex items-center text-sm text-gray-800 hover:text-gray-500 focus:outline-none focus:text-gray-500" href="pageblog.php">
+                        <a class="p-2 flex items-center text-sm text-blue-600 focus:outline-none focus:text-blue-600 " href="#">
+                            
                             <svg class="shrink-0 size-4 me-3 md:me-2 block md:hidden" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
                                 <circle cx="12" cy="7" r="4" />
@@ -151,107 +152,12 @@ session_start();
             <!-- End Collapse -->
         </nav>
     </header>
-    <section class="py-24 ">
-
-        <!-- Card Blog -->
-        <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-            <!-- Grid -->
-            <div class="grid sm:grid-cols-2 sm:items-center gap-8">
-                <div class="sm:order-2">
-                    <div class="relative pt-[50%] sm:pt-[100%] rounded-lg">
-                        <img class="size-full absolute top-0 start-0 object-cover rounded-lg" src="https://images.unsplash.com/photo-1606868306217-dbf5046868d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80" alt="Blog Image">
-                    </div>
-                </div>
-                <!-- End Col -->
-
-                <div class="sm:order-1">
-                    <p class="mb-5 inline-flex items-center gap-1.5 py-1.5 px-3 rounded-md text-xs font-medium bg-gray-100 text-gray-800">
-                        Business insight
-                    </p>
-
-                    <h2 class="text-2xl font-bold md:text-3xl lg:text-4xl lg:leading-tight xl:text-5xl xl:leading-tight text-gray-800">
-                        <a class="hover:text-blue-600 focus:outline-none focus:text-blue-600" href="#">
-                            How to get buy-in and budget for direct hiring
-                        </a>
-                    </h2>
-
-                    <!-- Avatar -->
-                    <div class="mt-6 sm:mt-10 flex items-center">
-                        <div class="shrink-0">
-                            <img class="size-10 sm:h-14 sm:w-14 rounded-full" src="https://images.unsplash.com/photo-1669837401587-f9a4cfe3126e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Blog Image">
-                        </div>
-
-                        <div class="ms-3 sm:ms-4">
-                            <p class="sm:mb-1 font-semibold text-gray-800">
-                                Louise Donadieu
-                            </p>
-                            <p class="text-xs text-gray-500">
-                                Strategic Marketing Consultant
-                            </p>
-                        </div>
-                    </div>
-                    <!-- End Avatar -->
-
-                    <div class="mt-5">
-                        <a class="inline-flex items-center gap-x-1.5 text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium" href="#">
-                            Read more
-                            <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                <path d="m9 18 6-6-6-6" />
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-                <!-- End Col -->
-            </div>
-            <!-- End Grid -->
-        </div>
-        <!-- End Card Blog -->
-        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <h2 class="font-manrope text-4xl font-bold text-gray-900 text-center mb-16">Our latest blog</h2>
-            <div class="flex justify-center  gap-y-8 lg:gap-y-0 flex-wrap md:flex-wrap lg:flex-nowrap lg:flex-row lg:justify-between lg:gap-x-8">
-                <div class="group w-full max-lg:max-w-xl lg:w-1/3 border border-gray-300 rounded-2xl">
-                    <div class="flex items-center">
-                        <img src="https://pagedone.io/asset/uploads/1696244317.png" alt="blogs tailwind section" class="rounded-t-2xl w-full object-cover">
-                    </div>
-                    <div class="p-4 lg:p-6 transition-all duration-300 rounded-b-2xl group-hover:bg-gray-50">
-                        <span class="text-indigo-600 font-medium mb-3 block">Jan 01, 2023</span>
-                        <h4 class="text-xl text-gray-900 font-medium leading-8 mb-5">Clever ways to invest in product to organize your portfolio</h4>
-                        <p class="text-gray-500 leading-6 mb-10">Discover smart investment strategies to streamline and organize your portfolio..</p>
-                        <a href="javascript:;" class="cursor-pointer text-lg text-indigo-600 font-semibold">Read more..</a>
-                    </div>
-                </div>
-                <div class="group w-full max-lg:max-w-xl lg:w-1/3 border border-gray-300 rounded-2xl">
-                    <div class="flex items-center">
-                        <img src="https://pagedone.io/asset/uploads/1696244340.png" alt="blogs tailwind section" class="rounded-t-2xl w-full object-cover">
-                    </div>
-                    <div class="p-4 lg:p-6 transition-all duration-300 rounded-b-2xl group-hover:bg-gray-50">
-                        <span class="text-indigo-600 font-medium mb-3 block">Feb 01, 2023</span>
-                        <h4 class="text-xl text-gray-900 font-medium leading-8 mb-5">How to grow your profit through systematic investment with us</h4>
-                        <p class="text-gray-500 leading-6 mb-10">Unlock the power of systematic investment with us and watch your profits soar. Our..</p>
-                        <a href="javascript:;" class="cursor-pointer text-lg text-indigo-600 font-semibold">Read more..</a>
-                    </div>
-                </div>
-                <div class="group w-full max-lg:max-w-xl lg:w-1/3 border border-gray-300 rounded-2xl">
-                    <div class="flex items-center">
-                        <img src="https://pagedone.io/asset/uploads/1696244356.png" alt="blogs tailwind section" class="rounded-t-2xl w-full object-cover">
-                    </div>
-                    <div class="p-4 lg:p-6 transition-all duration-300 rounded-b-2xl group-hover:bg-gray-50">
-                        <span class="text-indigo-600 font-medium mb-3 block">Mar 01, 20233</span>
-                        <h4 class="text-xl text-gray-900 font-medium leading-8 mb-5">How to analyze every holdings of your portfolio</h4>
-                        <p class="text-gray-500 leading-6 mb-10">Our comprehensive guide will equip you with the tools and insights needed to..</p>
-                        <a href="javascript:;" class="cursor-pointer text-lg text-indigo-600 font-semibold">Read more..</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    
 
 
 
 
 
-    <!-- ========== END HEADER ========== -->
-    <!-- JS Implementing Plugins -->
 
     <!-- JS PLUGINS -->
     <!-- Required plugins -->
